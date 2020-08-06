67d94136e275131f54c815a513f817de0adc1a9a
doniwirawan <doniwirawan166@gmail.com>
2020-08-06 08:58:34 +0800

    make responsive

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+$('#showAside').click(function(){
+    $('.sidenav').toggle();
+    $('.onclickshow').show();
+    $('.main').css("filter", "blur(5px)");
+    $('.navbar-lg-hidden').css("filter", "blur(5px)");
+})
+$('.onclickshow').click(function () {
+    $('.sidenav').hide();
+    $('.onclickshow').hide();
+    $('.main').css("filter", "blur(0)");
+    $('.navbar-lg-hidden').css("filter", "blur(0)");
+
+})
